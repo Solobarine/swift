e6c3a4c79366efabbafb923c2deb805dba5463be
Solomon <solobarine@gmail.com>
2023-04-08 21:23:09 +0100

    Fix linter errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
-    belongs_to :post
-    belongs_to :user, class_name: 'User'
-  
-    def update_like_counter
-      post.likes_counter = likes.count
-    end
+  belongs_to :post
+  belongs_to :user, class_name: 'User'
+
+  def update_like_counter
+    post.likes_counter = likes.count
   end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,18 @@
 class Post < ApplicationRecord
-    has_many :likes
-    has_many :comments
-    belongs_to :user, class_name: 'User'
-  
-    validates :title, presence: true
-    validates :title, length: { less_than_or_equal_to: 250 }
-    validates :comments_counter, numericality: true
-    validates :likes_counter, numericality: true
-  
-    def update_post_counter
-      user.posts_counter = post.count
-    end
-  
-    def recent_comment_for_post
-      comments.order(created_at: :desc).limit(5)
-    end
+  has_many :likes
+  has_many :comments
+  belongs_to :user, class_name: 'User'
+
+  validates :title, presence: true
+  validates :title, length: { less_than_or_equal_to: 250 }
+  validates :comments_counter, numericality: true
+  validates :likes_counter, numericality: true
+
+  def update_post_counter
+    user.posts_counter = post.count
   end
+
+  def recent_comment_for_post
+    comments.order(created_at: :desc).limit(5)
+  end
+end
