78f6c1086a91af1cf06e487c7c68b170d4a5e4d6
Solomon <solobarine@gmail.com>
2023-04-08 21:25:39 +0100

    Fix comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
-belongs_to :user, class_name: 'User'
-belongs_to :post
-
-def update_comment_counter
-  posts.comments_counter = comments.count
-end
-end
+class Comment < ApplicationRecord
+    belongs_to :user, class_name: 'User'
+    belongs_to :post
+  
+    def update_comment_counter
+      posts.comments_counter = comments.count
+    end
+  end
